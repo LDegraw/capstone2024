89603e9e85fd9150d5a66d9f16d3c8a0c6a6b906
Lukas DeGraw <LDegraw@unomaha.edu>
2024-03-22 22:12:17 -0500

    cameraMatrix

--- camOdometry.py
+++ camOdometry.py
@@ -5,6 +5,13 @@ import random
 import numpy as np
 import time
 
+
+x = 0
+y = 0
+
+lk_params = dict(winSize  = (21, 21), 
+				#maxLevel = 3,
+             	criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
 # Feedback for odemetry errors
 
 # TODO: Camera Calibration for raspberry pi
@@ -15,6 +22,7 @@ cam = cv2.VideoCapture(0)
 # setting to known resolution
 cam.set(3, 1280)
 cam.set(4, 720)
+focalLength = 500
 
 while True:
     start_time = time.time()
@@ -27,18 +35,26 @@ while True:
     # 3 more goals end of semester
 
     # creating two frames to read from
-    #_, frame2 = cam.read()
-    #gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    #keyPoints2 = fast.detect(gray2, None)
-
-    #cv2.calcOpticalFlowPyrLK(gray1, gray2, keyPoints1, keyPoints2)
+    _, frame2 = cam.read()
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    keyPoints2 = fast.detect(gray2, None)
+    
+    for i in keyPoints1:
+        
+    
+    kp2, st, err = cv2.calcOpticalFlowPyrLK(gray2, gray1, keyPoints1, keyPoints2, None, **lk_params)
+    st = st.reshape(st.shape[0])
+    kp1 = keyPoints1[st == 1]
+    kp2 = kp2[st == 1]
     # Need only 5 good points to calculate any distance from frame to frame to input into essential matrix
-    #cv2.findEssentialMat()
+    E, mask = cv2.findEssentialMat(keyPoints2, keyPoints1, focalLength, method=cv2.RANSAC, prob=0.999, threshold=1.0)
     # y_1^T * E * y = 0 for y being normalized image coordinates
     
     # recover rotation matrix
-    #recoverPose(E, kepoints1, keypoints2, R, t, focal, pp, mask)
+    _, R, T = cv2.recoverPose(E, keyPoints2, keyPoints1, focalLength, pp=(x,y))
 
+    print(R)
+    print(T)
     #using guassian blurring to make image more fuzzy/ better for detector
     blurring = cv2.GaussianBlur(gray1, (5,5), 0)
     print(len(keyPoints1))
@@ -51,3 +67,5 @@ while True:
     if cv2.waitKey(1)==ord('q'):
         break
 cam.release()
+
+
---
+++ cameraMatrix/camCalibrate.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+import numpy as np
+
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+a = 6
+b = 8
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((b*a,3), np.float32)
+objp[:,:2] = np.mgrid[0:a,0:b].T.reshape(-1,2)
+
+images = os.listdir("images")
+for photo in images:
+    print(photo)
+    img = cv2.imread("images/%s" % photo)
+
+    grayScale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Gray', grayScale)
+    cv2.moveWindow('Gray', 700, 0)
+
+    background = cv2.inRange(grayScale,100, 255)
+    
+    # setting region of interest
+    roi = background[100:600, 300:850]
+    cv2.imshow('edited', roi)
+    cv2.moveWindow('edited',350, 300)
+
+    ret, corners = cv2.findChessboardCorners(grayScale, (a,b),  None)
+
+    if ret == True:
+        print("Appended!")
+        objpoints.append(objp)
+        imgpoints.append(corners)
+        drawn = cv2.drawChessboardCorners(img, (a,b), corners, ret)
+    else:
+        print("FAILED: Image %s" % photo)
+    
+    cv2.imshow('img', img)
+    cv2.waitKey(500)
+
+cv2.destroyAllWindows()
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, grayScale.shape[::-1], None, None)
+dst = cv2.undistort(img, mtx, dist, None)
+cv2.imshow('img', img)
+cv2.moveWindow('img', 500, 100)
+cv2.imshow('undistorted image', dst)
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
+print("Camera Calibrated: ", ret)
+print("\nCamera Matrix:\n", mtx)
+print("\nDistortion Parameters\n", dist)
+print("\nRotation Vectors:\n", rvecs)
+print("\nTranslation Vectors:\n", tvecs)
+mean_error = 0
+for i in range(len(objpoints)):
+ imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+ error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+ mean_error += error
+ 
+print( "total error: {}".format(mean_error/len(objpoints)) )
